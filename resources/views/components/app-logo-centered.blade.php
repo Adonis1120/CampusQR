e54905e6a6894b9e7393bf284c0e85a3881388bb
Adonis Imperial <adonisimperial@yahoo.com>
2025-04-21 14:26:23 +0800

    scssLogoAtcentered-iconClass

--- resources/views/components/app-logo-centered.blade.php
+++ resources/views/components/app-logo-centered.blade.php
@@ -1,6 +1,6 @@
-<a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
-    <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">
-        <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+<a href="{{ route('home') }}" class="centered-icon" wire:navigate>
+    <span class="centered-icon-span">
+        <x-app-logo-icon class="cetered-icon-item" />
     </span>
     <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
 </a>
